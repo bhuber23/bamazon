4b1f9ddbccab5a25170241d89b337a57cf0f0038
Brittany Huber <brittanyleighhuber@gmail.com>
2020-01-01 22:12:38 -0700

    Added low inventory function

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,12 +1,13 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var {table} = require("table");
 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "Password",
-    database: bamazon
+    database: "bamazon"
 });
 
 connection.connect(function(err){
@@ -35,4 +36,37 @@ function start(){
             addProduct();
         }
     });
+}
+
+function viewProducts(){
+    var query = "SELECT * FROM products";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.log("Here is the existing inventory:");
+        console.log("\n--------------------------------\n");
+        var products = [["Item ID", "Product Name", "Department", "Price", "Quantity"]];
+        for (var i = 0; i < res.length; i++){
+            products.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity])
+        }
+        console.log(table(products));
+        start();
+    });
+}
+
+//View inventory with stock less than 5
+function lowInventory(){
+    var query = "SELECT * FROM products WHERE stock_quantity < 5";
+    connection.query(query, function(err, res){
+        if (err) throw err;
+        console.log("Here are the items that are low on inventory:")
+        console.log("\n--------------------------------\n");
+
+        var products = [["Item ID", "Product Name", "Price", "Quantity"]];
+        for (var i = 0; i < res.length; i++){
+            products.push([res[i].item_id, res[i].product_name, res[i].price, res[i].stock_quantity])
+        }
+        console.log(table(products));
+        start();
+
+    })
 }
