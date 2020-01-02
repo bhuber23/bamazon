0411b87aa066002619e319f5b89b9cfdbecc7f89
Brittany Huber <brittanyleighhuber@gmail.com>
2020-01-01 18:28:09 -0700

    Added table and products in seed file

---
+++ seed.sql
@@ -0,0 +1,14 @@
+SELECT * FROM products;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES 
+("Nintendo Switch", "Electronics", 300.00, 3),
+("Anastasia Liquid Lipstick", "Cosmetics", 20.00, 10),
+("Avengers End Game", "Movies", 19.99, 6),
+("Pokemon Shield", "Electronics", 42.98, 2),
+("Playstation 4", "Electronics", 299.99, 13),
+("Sultry Palette", "Cosmetics", 45.00, 8),
+("The Lion King", "Movies", 12.46, 1),
+("Spatula", "Kitchen", 2.46, 32),
+("Wine Glass", "Kitchen", 42.67, 2),
+("Mario Kart", "Electronics", 48.50, 4);
